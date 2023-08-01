72c617dcb90ae1da792cbcf15a2ba6264e6e621b
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-08-01 22:18:44 +0530

    react project

--- react-recipe/src/App.js
+++ react-recipe/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: flex;
+flex-direction:column;
+`;
+const Header = styled.civ`
+color: white;
+background-color:black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Recipe Finder</Header>
+     The  Indian Dev
+    </Container>
   );
 }
 
